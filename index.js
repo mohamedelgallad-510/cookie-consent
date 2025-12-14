9a9ed0312893e1b51549a823d2b2c4143a2f6f82
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2025-12-14 21:34:15 +0300

    Updated pop-up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -12,4 +13,22 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    modalText.innerHTML = `<div class="modal-inner-loading">
+                            <img src="images/loading.svg" class="loading">
+                            <p id="uploadText">
+                                Uploading your data to the dark web...
+                            </p>
+                           </div>`
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = "Making the sale..."
+    }, 1500)
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    ` 
+    }, 3000)
 })
