582e599cf5a19acf614b6e213de0ecbd85017794
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2025-12-15 23:15:57 +0300

    Added name to pop-up from form

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+    const consentFormData = new FormData(consentForm)
+    const name = consentFormData.get('fullName')    
     modalText.innerHTML = `<div class="modal-inner-loading">
                             <img src="images/loading.svg" class="loading">
                             <p id="uploadText">
@@ -24,7 +26,7 @@ consentForm.addEventListener('submit', function(e){
     }, 1500)
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
